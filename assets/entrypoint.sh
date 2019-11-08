95572718a9f28ef4bbeb858d0660db09bc1430ea
Everton de Vargas Agilar <evertonagilar@ufsm.br>
2019-11-08 11:01:54 -0300

    build

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -23,10 +23,11 @@ echo '200.132.39.65 alpha.cpd.ufsm.br' 		>> /etc/hosts
 echo -e "\033[00;37mServidor da base de conhecimento do \033[01;34mCPD/UFSM\033[01;37m"
 cd /var/opt
 #git clone -q --depth 'https://evertonagilar:unb960101$$$@alpha.cpd.ufsm.br/desenvolvimento/knowledge-base'
-GIT_PASSWD='unb960101$$$'
-echo 'Fazendo download do projeto...'
-git clone -q --depth 1 https://evertonagilar:$GIT_PASSWD@alpha.cpd.ufsm.br/desenvolvimento/knowledge-base
-echo 'Entrando no projeto'
+echo 'Fazendo download do projeto knowledge-base do GitLab...'
+git clone --depth 1 https://$GIT_USER:$GIT_PASSWD@alpha.cpd.ufsm.br/desenvolvimento/knowledge-base
+echo 'Entrando no projeto knowledge-base...'
 cd knowledge-base/src
+echo 'Ativando crond'
 crond
+echo 'Iniciando jekyll...'
 jekyll serve
